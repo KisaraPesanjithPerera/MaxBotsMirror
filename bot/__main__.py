5451e54a3c876dfdd7001ed33299d439308c0e8d
Kisara Pesanjith Perera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-08 21:51:56 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,7 +45,7 @@ def start(update, context):
     start_string = f'''
 This is a bot which can mirror all your links to Google drive!
 
-👲 Moded By: @MaxxRiderz
+😎 Moded By: @MaX_Bots
 
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
